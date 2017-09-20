dd64aa1d5ede2cf56a3512f33a079c4d1dbfe3ad
shashi693 <sukenkar@hotmail.com>
2017-09-20 10:31:03 +0530

    Wifi & North dir
    Wifi & North dir

--- mobile/src/main/java/com/avenueinfotech/locationviewpager/MainActivity.java
+++ mobile/src/main/java/com/avenueinfotech/locationviewpager/MainActivity.java
@@ -1,9 +1,16 @@
 package com.avenueinfotech.locationviewpager;
 
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
+import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -11,9 +18,17 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.ActivityRecognition;
@@ -35,7 +50,7 @@ import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener, OnMapReadyCallback, LocationListener, GoogleMap.OnMyLocationButtonClickListener {
+        GoogleApiClient.OnConnectionFailedListener, OnMapReadyCallback, LocationListener, GoogleMap.OnMyLocationButtonClickListener, SensorEventListener {
 
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
 
@@ -51,10 +66,35 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
     private static final LatLng SYDNEY = new LatLng(-33.8688, 151.2091);
 
+    private AutoCompleteAdapter mAdapter;
+
+    ImageView iv_arrow;
+    TextView tv_degrees;
+
+    private static SensorManager sensorService;
+    private Sensor sensor;
+
+    private float currentDegree = 0f;
+
+    WifiManager wifiManager;
+    TextView wifiStatus;
+    Switch wifiSwitch;
+//    private static GPSTracker gps;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MobileAds.initialize(getApplicationContext(), "ca-app-pub-1183672799205641~1617315814");
+
+//        mTextView = (TextView) findViewById(R.id.textview);
+
+        iv_arrow = (ImageView)findViewById(R.id.iv_arrow);
+        tv_degrees = (TextView)findViewById(R.id.tv_degrees);
+
+        sensorService = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensor = sensorService.getDefaultSensor(Sensor.TYPE_ORIENTATION);
+
 
         mApiClient = new GoogleApiClient.Builder(this)
                 .addApi(ActivityRecognition.API)
@@ -63,10 +103,28 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                 .addOnConnectionFailedListener(this)
                 .build();
 
+        // Load an ad into the AdMob banner view.
+        AdView adView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder()
+                .setRequestAgent("android_studio:ad_template").build();
+        adView.loadAd(adRequest);
+
 
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        wifiManager = (WifiManager) getApplicationContext().getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+//        wifiSwitch = (Switch)findViewById(R.id.wifiswitch);
+        wifiStatus = (TextView)findViewById(R.id.wifistatus);
+
+        if (wifiManager.isWifiEnabled()){
+//            wifiSwitch.setChecked(true);
+            wifiStatus.setText("Wifi ON");
+        } else {
+//            wifiSwitch.setChecked(false);
+            wifiStatus.setText("Wifi OFF");
+        }
     }
 
     LocationRequest mLocationRequest;
@@ -102,22 +160,64 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     }
 
     @Override
-    public void onMapReady(GoogleMap map) {
-        mMap = map;
-        //      mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SYDNEY, 14));
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
+    protected void onStart() {
+        super.onStart();
+        if (mApiClient != null)
+            mApiClient.connect();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(sensor != null){
+            sensorService.registerListener(this, sensor, SensorManager.SENSOR_DELAY_FASTEST);
+
+        }else {
+            Toast.makeText(MainActivity.this, "Not supported", Toast.LENGTH_LONG).show();
         }
-        mMap.setMyLocationEnabled(true);
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorService.unregisterListener(this);
+    }
 
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        int degree = Math.round(sensorEvent.values[0]);
+
+        tv_degrees.setText(Integer.toString(degree) + (char) 0x00B0);
+
+        RotateAnimation rs = new RotateAnimation(currentDegree, -degree,
+                Animation.RELATIVE_TO_SELF, 0.5f,Animation.RELATIVE_TO_SELF, 0.5f);
+
+        rs.setDuration(1000);
+        rs.setFillAfter(true);
+
+        iv_arrow.startAnimation(rs);
+        currentDegree = -degree;
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    protected void onStop() {
+        if (mApiClient != null && mApiClient.isConnected()) {
+            mAdapter.setGoogleApiClient(null);
+            mApiClient.disconnect();
+        }
+        super.onStop();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+//        goToLocationZoom(39.25, -75.896, 15);
 
         if (mMap != null) {
 
@@ -153,37 +253,46 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                 }
             });
 
-//            mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
-//                @Override
-//                public View getInfoWindow(Marker marker) {
-//                    return null;
-//                }
-//
-//                @Override
-//                public View getInfoContents(Marker marker) {
-////                    View v = getLayoutInflater().inflate(R.layout.info_window, null);
-////
-////                    TextView tvLocality = (TextView) v.findViewById(R.id.tv_locality);
-////                    TextView tvLat = (TextView) v.findViewById(R.id.tv_lat);
-////                    TextView tvLng = (TextView) v.findViewById(R.id.tv_lng);
-////                    TextView tvSnippet = (TextView) v.findViewById(R.id.tv_snippet);
-//
-//
-////                    LatLng ll = marker.getPosition();
-////                    tvLocality.setText(marker.getTitle());
-////                    tvLat.setText("Latiude: " + ll.latitude);
-////                    tvLng.setText("Longitude: " + ll.longitude);
-////                    tvSnippet.setText(marker.getSnippet());
-//
-//
-////                    return v;
-//                }
-//            });
+            mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+                @Override
+                public View getInfoWindow(Marker marker) {
+                    return null;
+                }
+
+                @Override
+                public View getInfoContents(Marker marker) {
+                    View v = getLayoutInflater().inflate(R.layout.info_window, null);
+
+                    TextView tvLocality = (TextView) v.findViewById(R.id.tv_locality);
+                    TextView tvLat = (TextView) v.findViewById(R.id.tv_lat);
+                    TextView tvLng = (TextView) v.findViewById(R.id.tv_lng);
+//                    TextView tvSnippet = (TextView) v.findViewById(R.id.tv_snippet);
+
+
+                    LatLng ll = marker.getPosition();
+                    tvLocality.setText(marker.getTitle());
+                    tvLat.setText("Latiude: " + ll.latitude);
+                    tvLng.setText("Longitude: " + ll.longitude);
+//                    tvSnippet.setText(marker.getSnippet());
+
+
+                    return v;
+                }
+            });
         }
 
         mMap.setOnMyLocationButtonClickListener(this);
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+        }
+        mMap.setMyLocationEnabled(true);
+
         mMap.getUiSettings().setZoomControlsEnabled(true);
-        enableMyLocation();
+
     }
 
     /**
@@ -243,7 +352,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
         double lat = address.getLatitude();
         double lng = address.getLongitude();
-        goToLocationZoom(lat, lng, 25);
+        goToLocationZoom(lat, lng, 22);
 
         setMarker(locality, lat, lng);
 
@@ -257,7 +366,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         MarkerOptions options = new MarkerOptions()
                 .title(locality)
                 .draggable(true)
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.search_icon))
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.smartsearch))
                 .position(new LatLng(lat, lng))
                 .snippet("I am here");
 
